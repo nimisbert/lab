88f003617db9eb0e92478835bc03a634252f3a7c
nimisbert <n.misbert@yahoo.fr>
2023-09-23 20:59:43 +0200

    euler problem 3

--- .euler/3.hs
+++ .euler/3.hs
@@ -1,1 +1,16 @@
 -- Largest Prime Factor of 600851475143
+-- Test 13195 -> 5, 7, 13, 29
+
+-- Finding prime factors by trial division method
+
+trialDivisionHelper :: Int -> Int -> [Int]
+trialDivisionHelper f n 
+    | n <= 1          = []
+    | n `mod` f == 0 = f:trialDivisionHelper f (n `div` f)
+    | otherwise      = trialDivisionHelper (f+1) n
+
+trialDivision :: Int -> [Int]
+trialDivision = trialDivisionHelper 2
+
+answer :: Int 
+answer = maximum $ trialDivision 600851475143
