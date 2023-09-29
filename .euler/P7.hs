1ca78cb736e6d5ecdaebfd8b45605bc105b2222b
nimisbert <n.misbert@yahoo.fr>
2023-09-29 20:51:50 +0200

    euler problem 7

--- .euler/P7.hs
+++ .euler/P7.hs
@@ -1,10 +1,22 @@
 -- Find the 10 001st prime number
 
--- Haskell's wiki article on pirme numbers
+answer :: Int
+answer = last $ take 10001 primes
 
-import Data.List ((\\), minus, union, unionAll )
+-- haskell.org on prime numbers
+primes = eratos [2..]
+    where eratos [] = []
+          eratos (p:xs) = p : eratos (xs `minus` [p, p+p ..])
 
-primesTo m = naiveSieve [2 .. m] -- m, bounded
-    where naiveSieve (x:xs) = x : naiveSieve ( xs \\ [x, x+x .. m] ) -- 
-          naiveSieve [] = []
-
+-- old, ordered lists, difference and union
+minus (x:xs) (y:ys) = case (compare x y) of 
+           LT -> x : minus  xs  (y:ys)
+           EQ ->     minus  xs     ys 
+           GT ->     minus (x:xs)  ys
+minus  xs     _     = xs
+union (x:xs) (y:ys) = case (compare x y) of 
+           LT -> x : union  xs  (y:ys)
+           EQ -> x : union  xs     ys 
+           GT -> y : union (x:xs)  ys
+union  xs     []    = xs
+union  []     ys    = ys
