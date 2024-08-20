47efc61e994aa67efd68633482541a0592e0f911
nicolas <nicolas@jolliet19.seca.local>
2024-08-20 15:54:08 +0200

    plot file format & gnuplot script for Maclaurin approx.

--- maclaurin/src/test.jl
+++ maclaurin/src/test.jl
@@ -1,10 +1,10 @@
 using Printf
 include("./maclaurin.jl")
 
-p = 5
+p = 3
 io = open("test.dat", "w")
 for i in -1:0.1:1
-    @printf( io, "%d %f %f %f %f %f %f %f %f %f %f %f %f\r\n", i, 
+    @printf( io, "%10f %10f %10f %10f %10f %10f %10f %10f %10f %10f %10f %10f %10f\r\n", i, 
              exp(i),   Maclaurin.exp(i,p),
              log1p(i), Maclaurin.log1p(i,p),
              sin(i),   Maclaurin.sin(i,p),
