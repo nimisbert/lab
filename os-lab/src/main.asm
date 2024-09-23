63c74d32597316c10615d81c1daea0f269f74f0a
nimisbert <n.misbert@yahoo.fr>
2024-09-23 19:39:20 +0200

    bad assembly startup

--- os-lab/src/main.asm
+++ os-lab/src/main.asm
@@ -1,9 +1,33 @@
 org 0x7C00
 bits 16
 
-
 main:
-    hlt
+    ; set up data segments
+    mov ax, 0   ; can't write to ds/es directly
+    mov ds, ax 
+    mov es, ax
+    ; set up stack segment
+    mov ss, ax
+    mov sp, 0x7C00
+
+
+; @brief Prints a caracter to the screen
+; @param string loaded in ds:si
+puts:
+    ; save registers we will modify in stack
+    push si
+    push ax
+
+.loop:
+    lodsb      ; load the next caracter from ds:si to ax
+    or al al   ; verify if next caracter is null
+    .jz done   ; jump to destination if zero flag set by condition
+    .jmp .loop ; jump to loop if zero flag is not set 
+
+.done: 
+    pop ax
+    pop si
+    ret
 
 .halt:
     jmp .halt
